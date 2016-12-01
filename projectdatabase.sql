9cbd82d46b7afff3fb38ae22953e7ec0dfec74e6
aidanBailey <aidan.bailey@ucalgary.ca>
2016-12-01 16:41:58 -0700

    All Products Done

--- projectdatabase.sql
+++ projectdatabase.sql
@@ -23,8 +23,8 @@ CREATE TABLE `category` (
 #
 
 INSERT INTO `category` VALUES ('Furniture'),('Movies'),('Food'),('Clothing'),
-('Jewelery'), ('Automotive'), ('Assorted Trinkets'),('Music'), ('Electronics'), ('Video Games'), 
-('Toys'), ('Furnaces'), ('Sports & Outdoors');
+('Jewelery'), ('Automotive'), ('Assorted_Trinkets'),('Music'), ('Electronics'), ('Video_Games'), 
+('Toys'), ('Furnaces'), ('Sports_&_Outdoors');
 
 
 #
@@ -95,8 +95,9 @@ CREATE TABLE `supplier` (
 # Data for table "supplier"
 #
 
-INSERT INTO `supplier` VALUES ('BigSupplier', 20), ('Microsoft', 20), 
-('Unilever', 50), ('Coca-Cola', 20), ('The_Brick', 50);
+INSERT INTO `supplier` VALUES ('BigSupplier', 20), ('Microsoft', 20), ('The_Brick', 50), ('Warner_Bros.', 10),
+('Spence_Diamonds', 5), ('Ford', 40), ('EMI', 19), ('Bethesda', 10), ('Mattel', 20),
+('Big_Furnaces_America', 3), ('Cabellas', 10);
 
 #
 # Structure for table "product"
@@ -124,7 +125,40 @@ CREATE TABLE `product` (
 
 INSERT INTO `product` VALUES (1, 'TurboFlax', 10, 50, 0, 'Food', 'BigSupplier'), 
 (2, 'Broccoli', 5, 200, 0, 'Food', 'BigSupplier'), (3, 'Oranges', 3, 100, 0, 'Food', 'BigSupplier'),
-(4, 'Chair', 100, 30, 0, 'Furniture', 'The_Brick');
+(4, 'Chair', 100, 30, 0, 'Furniture', 'The_Brick'), (5, 'Couch', 200, 30, 0, 'Furniture', 'The_Brick'),
+(6, 'Table', 300, 20, 0, 'Furniture', 'The_Brick'), (7, 'The_Lone_Ranger', 1, 3, 0, 'Movies', 'Warner_Bros.'),
+(8, 'Max_Payne', 5, 10, 0, 'Movies', 'Warner_Bros.'), (9, 'Les_Miserables', 500, 37, 0, 'Movies', 'Warner_Bros.');
+INSERT INTO `product` VALUES (10, 'Loose_Shirt', 13, 100, 0, 'Clothing', 'BigSupplier');
+INSERT INTO `product` VALUES (11, 'Jean_Shorts', 1, 35, 0, 'Clothing', 'BigSupplier');
+INSERT INTO `product` VALUES (12, 'Baseball_Cap', 35, 20, 0, 'Clothing', 'BigSupplier');
+INSERT INTO `product` VALUES (13, 'Magic_Pear', 500, 5, 0, 'Jewelery', 'Spence_Diamonds');
+INSERT INTO `product` VALUES (14, 'Hope_Diamond', 15, 7, 0, 'Jewelery', 'Spence_Diamonds');
+INSERT INTO `product` VALUES (15, 'Ruby_Encrusted_Fish', 50000, 100, 0, 'Jewelery', 'Spence_Diamonds');
+INSERT INTO `product` VALUES (16, 'Bumper', 200, 15, 0, 'Automotive', 'Ford');
+INSERT INTO `product` VALUES (17, 'Wheel', 100, 7, 0, 'Automotive', 'Ford');
+INSERT INTO `product` VALUES (18, 'Hood', 600, 15, 0, 'Automotive', 'Ford');
+INSERT INTO `product` VALUES (19, 'PushPin', 4, 20, 0, 'Assorted_Trinkets', 'BigSupplier');
+INSERT INTO `product` VALUES (20, 'Mexican_Guitar', 100, 10, 0, 'Assorted_Trinkets', 'BigSupplier');
+INSERT INTO `product` VALUES (21, 'Shot_Glass', 3, 30, 0, 'Assorted_Trinkets', 'BigSupplier');
+INSERT INTO `product` VALUES (22, 'The_Beatles:White_Album', 15, 20, 0, 'Music', 'EMI');
+INSERT INTO `product` VALUES (23, 'Radiohead:OK_Computer', 15, 20, 0, 'Music', 'EMI');
+INSERT INTO `product` VALUES (24, 'PDiddy_Last_Train_To_Paris', 4, 20, 0, 'Music', 'EMI');
+INSERT INTO `product` VALUES (25, 'Surface_4', 1000, 30, 0, 'Electronics', 'Microsoft');
+INSERT INTO `product` VALUES (26, 'Microsoft_Office', 300, 20, 0, 'Electronics', 'Microsoft');
+INSERT INTO `product` VALUES (27, 'Motorola_RAZR', 1000, 30, 0, 'Electronics', 'BigSupplier');
+INSERT INTO `product` VALUES (28, 'Fallout4', 50, 30, 0, 'Video_Games', 'Bethesda');
+INSERT INTO `product` VALUES (29, 'Fallout3', 30, 30, 0, 'Video_Games', 'Bethesda');
+INSERT INTO `product` VALUES (30, 'Skyrim', 50, 30, 0, 'Video_Games', 'Bethesda');
+INSERT INTO `product` VALUES (31, 'Hot_Wheels', 10, 30, 0, 'Toys', 'Mattel');
+INSERT INTO `product` VALUES (32, 'Barbie', 15, 30, 0, 'Toys', 'Mattel');
+INSERT INTO `product` VALUES (33, 'Fisher-Price', 55, 20, 0, 'Toys', 'Mattel');
+INSERT INTO `product` VALUES (34, 'Super_Furnace_3000', 3001, 20, 0, 'Furnaces', 'Big_Furnaces_America');
+INSERT INTO `product` VALUES (35, 'Garbage_Incinerator', 403, 20, 0, 'Furnaces', 'Big_Furnaces_America');
+INSERT INTO `product` VALUES (36, 'Uncontrolled_Fire_Blaster', 2000000, 20, 0, 'Furnaces', 'Big_Furnaces_America');
+INSERT INTO `product` VALUES (37, 'Tent', 100, 10, 0, 'Sports_&_Outdoors', 'Cabellas');
+INSERT INTO `product` VALUES (38, 'Jet_Boil', 50, 15, 0, 'Sports_&_Outdoors', 'Cabellas');
+INSERT INTO `product` VALUES (39, 'Axe', 12, 5, 0, 'Sports_&_Outdoors', 'Cabellas');
+
 
 
 #
