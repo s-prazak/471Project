4b355b939c0c84a978d689f3406df72e2bb78710
Spencer <prazakspencer@gmail.com>
2016-12-06 16:24:13 -0700

    Adding credentials to login page

--- loginPage.php
+++ loginPage.php
@@ -6,13 +6,27 @@
     </head>
     <body>
         <h1>Login</h1>
-        <form action="adminPage.php">
+        <form>
             Username:<br>
-        <input type="text" name="username"><br>
+        <input type="text" name="username" id="username" required><br>
             Password:<br>
-        <input type="text" name="password"><br>
-        <input type="submit" value="Submit"><br>
+        <input type="password" name="password" id="password" required><br>
+        <input type="submit" value="Submit" onclick="return checkLogin()"><br>
         </form>
     </body>
 </html>
+
+<script type="text/javascript">
+    checkLogin = function() {
+        var username = document.getElementById("username").value;
+        var password = document.getElementById("password").value;
+        if(username === 'admin' && password === 'admin') {
+            location.href = 'adminPage.php';
+            return false;
+        }
+        else {
+            alert("Invalid login information, please try again.");
+        }
+    }
+</script>
 
